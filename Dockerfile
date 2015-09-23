d293d678bc94ad972a6a4bda76146301d537b90c
Florian Motlik <flomotlik@gmail.com>
2015-09-23 11:40:45 +0200

    Create Google Container and Add Container engine examples

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,25 @@
-FROM google/cloud-sdk:latest
+FROM debian:jessie
 
-WORKDIR /app
+# Copied from https://hub.docker.com/r/google/cloud-sdk/~/dockerfile/
+# If there are upstrem changes copy them here
 
-COPY authenticate_and_run.sh ./
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update && apt-get install -y -qq --no-install-recommends wget unzip python php5-mysql php5-cli php5-cgi openjdk-7-jre-headless openssh-client python-openssl && apt-get clean
+
+RUN wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.zip && unzip google-cloud-sdk.zip && rm google-cloud-sdk.zip
+ENV CLOUDSDK_PYTHON_SITEPACKAGES 1
+RUN google-cloud-sdk/install.sh --usage-reporting=true --path-update=true --bash-completion=true --rc-path=/.bashrc --disable-installation-options
+RUN google-cloud-sdk/bin/gcloud --quiet components update pkg-go pkg-python pkg-java preview alpha beta app
+RUN google-cloud-sdk/bin/gcloud --quiet config set component_manager/disable_update_check true
+
+RUN mkdir /.ssh
+ENV PATH /google-cloud-sdk/bin:$PATH
+ENV HOME /
+
+# ----------
+
+RUN gcloud components update kubectl
+
+RUN echo "deb http://http.debian.net/debian jessie-backports main" > /etc/apt/sources.list.d/docker.list
+
+RUN apt-get update && apt-get install -y -qq docker.io
--- authenticate_and_run.sh
+++ authenticate_and_run.sh
@@ -2,44 +2,83 @@
 
 set -e
 
+GOOGLE_CONTAINER_NAME=gcr.io/codeship-production/google-deployment-example
+KUBERNETES_APP_NAME=google-deployment
+BUCKET_NAME=gs://codeship-production-testbucket
+UPLOAD_FILE_NAME=$BUCKET_NAME/datefile
+DEFAULT_ZONE=us-central1-a
+
 # Writing environment variable to Keyfile so it can be loaded later on
 echo $GOOGLE_KEY > keyconfig.json
 gcloud auth activate-service-account $GOOGLE_AUTH_EMAIL --key-file ./keyconfig.json --project $GOOGLE_PROJECT_NAME
 
-
-BUCKET_NAME=gs://codeship-production-testbucket
-UPLOAD_FILE_NAME=$BUCKET_NAME/datefile
-
 # Set the default zone to use
-gcloud config set compute/zone us-central1-a
+echo "Setting default timezone $DEFAULT_ZONE"
+gcloud config set compute/zone $DEFAULT_ZONE
 
-# Remove an existing bucket if it exists
+echo "Docker Images"
+docker images
+
+echo "Tagging the Docker machine for Google Container Registry push"
+docker tag -f codeship/google-deployment-example $GOOGLE_CONTAINER_NAME
+
+echo "Pushing to Google Container Registry: $GOOGLE_CONTAINER_NAME"
+gcloud docker push $GOOGLE_CONTAINER_NAME
+
+echo "Starting Cluster on GCE for $KUBERNETES_APP_NAME"
+gcloud container clusters create $KUBERNETES_APP_NAME \
+    --num-nodes 1 \
+    --machine-type g1-small
+
+echo "Deploying image on GCE"
+kubectl run $KUBERNETES_APP_NAME --image=$GOOGLE_CONTAINER_NAME --port=8080
+
+echo "Exposing a port on GCE"
+kubectl expose rc $KUBERNETES_APP_NAME --create-external-load-balancer=true
+
+echo "Waiting for services to boot"
+
+echo "Listing services on GCE"
+kubectl get services $KUBERNETES_APP_NAME
+
+echo "Removing service $KUBERNETES_APP_NAME"
+kubectl delete services $KUBERNETES_APP_NAME
+
+echo "Waiting After Remove"
+
+echo "Stopping port forwarding for $KUBERNETES_APP_NAME"
+kubectl stop rc $KUBERNETES_APP_NAME
+
+echo "Stopping Container Cluster for $KUBERNETES_APP_NAME"
+gcloud container clusters delete $KUBERNETES_APP_NAME -q
+
+echo "Remove an existing bucket if it exists"
 gsutil rb $BUCKET_NAME | true
 
-# Creating a bucket on Google Cloud Storage
+echo "Creating a bucket on Google Cloud Storage"
 gsutil mb $BUCKET_NAME
 
-# Create a file to upload to Google Cloud Storage
+echo "Create a file to upload to Google Cloud Storage"
 date > datefile
-# Upload file to Google Cloud Storage
+echo "Upload file to Google Cloud Storage"
 gsutil cp ./datefile $BUCKET_NAME
 
-# Download file from Google Cloud Storage for diff
+echo "Download file from Google Cloud Storage for diff"
 gsutil cp $UPLOAD_FILE_NAME ./datefile_downloaded
 
-# Removing file from Google Cloud Storage
+echo "Removing file from Google Cloud Storage"
 gsutil rm $UPLOAD_FILE_NAME
 
-# Removing Bucket on Google Cloud Storage
+echo "Removing Bucket on Google Cloud Storage"
 gsutil rb $BUCKET_NAME
 
-# Generating diff from local file and downloaded file
+echo "Generating diff from local file and downloaded file"
 diff datefile datefile_downloaded
 
-# TESTING INTERACTION WITH GOOGLE COMPUTE ENGINE
+echo "TESTING INTERACTION WITH GOOGLE COMPUTE ENGINE"
 
-# Starting an Instance in Google Compute Engine
+echo "Starting an Instance in Google Compute Engine"
 gcloud compute instances create testmachine
 
-# Stopping an Instance in Google Compute Engine
+echo "Stopping an Instance in Google Compute Engine"
 gcloud compute instances delete testmachine -q
