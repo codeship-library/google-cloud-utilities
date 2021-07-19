d5ece01989d6704bfd5e9cad62c7d0c85bbed773
Andrew Bayer <andrew.bayer@gmail.com>
2021-07-19 16:11:05 -0400

    Don't get stuck waiting for user input
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- dockercfg-generator/gcr_docker_creds.sh
+++ dockercfg-generator/gcr_docker_creds.sh
@@ -13,7 +13,8 @@ gcloud auth activate-service-account "${GOOGLE_AUTH_EMAIL}" --key-file /keyconfi
 
 # Syncing credentials
 echo "Syncing GCR credentials"
-gcloud auth configure-docker
+gcloud auth configure-docker -q
 
 echo "Writing Docker creds to $1"
-cat ~/.dockercfg > $1
+chmod 544 ~/.docker/config.json
+cp ~/.docker/config.json $1
