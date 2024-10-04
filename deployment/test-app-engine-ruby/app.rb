304e728dc83297646dc063058989810c06268cf2
Joe Siewert <jsiewert@cloudbees.com>
2024-10-04 12:45:08 -0500

    Fix Ruby test app

--- deployment/test-app-engine-ruby/app.rb
+++ deployment/test-app-engine-ruby/app.rb
@@ -1,4 +1,4 @@
-# Copyright 2015 Google, Inc
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START app]
+# [START gae_flex_quickstart]
 require "sinatra"
 
 get "/" do
   "Hello world!"
 end
-# [END app]
+# [END gae_flex_quickstart]
